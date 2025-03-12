71322f6359cbdb0d99eb3f23e017137cfb5f4ee4
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-03-12 11:57:00 -0300

    Atualiza backup

---
+++ dump/tecinternet_escola_kelly-etapa04-12-03-final.sql
@@ -0,0 +1,191 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 12/03/2025 às 15:53
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `tecinternet_escola_kelly`
+--
+CREATE DATABASE IF NOT EXISTS `tecinternet_escola_kelly` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `tecinternet_escola_kelly`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `alunos`
+--
+
+CREATE TABLE `alunos` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(120) NOT NULL,
+  `data_nasc` date DEFAULT NULL,
+  `nota_1` decimal(4,2) DEFAULT NULL,
+  `nota_2` decimal(4,2) DEFAULT NULL,
+  `curso_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `alunos`
+--
+
+INSERT INTO `alunos` (`id`, `nome`, `data_nasc`, `nota_1`, `nota_2`, `curso_id`) VALUES
+(2, 'Maria Souza', '1995-03-22', 5.72, 3.60, 2),
+(4, 'Ana Costa', '1998-01-28', 6.14, 10.00, 4),
+(6, 'Joana Silva', '1994-04-19', 4.88, 7.93, 2),
+(8, 'Gabriel Santos', '2000-12-25', 7.35, 9.68, 4),
+(9, 'Ricardo Martins', '1996-05-30', 3.60, 5.72, 1),
+(11, 'Lucas Almeida', '1993-07-12', 8.45, 8.02, 1),
+(12, 'Maria Souza', '1995-03-22', 5.72, 3.60, 2),
+(14, 'Ana Costa', '1998-01-28', 6.14, 10.00, 4),
+(15, 'Carlos Pereira', '2003-09-16', 7.93, 4.88, 5),
+(16, 'Joana Silva', '1994-04-19', 4.88, 7.93, 2),
+(17, 'Fernanda Rocha', '1999-08-02', 10.00, 6.14, 3),
+(18, 'Gabriel Santos', '2000-12-25', 7.35, 9.68, 4),
+(19, 'Ricardo Martins', '1996-05-30', 3.60, 5.72, 1),
+(20, 'Juliana Ferreira', '1997-10-09', 8.02, 7.45, 5),
+(21, 'Lucas Almeida', '1993-07-12', 8.45, 8.02, 1),
+(22, 'Maria Souza', '1995-03-22', 5.72, 3.60, 2),
+(23, 'Pedro Oliveira', '2001-11-05', 9.68, 7.35, 3),
+(24, 'Ana Costa', '1998-01-28', 6.14, 10.00, 4),
+(25, 'Carlos Pereira', '2003-09-16', 7.93, 4.88, 5),
+(26, 'Joana Silva', '1994-04-19', 4.88, 7.93, 2),
+(27, 'Fernanda Rocha', '1999-08-02', 10.00, 6.14, 3),
+(28, 'Gabriel Santos', '2000-12-25', 7.35, 9.68, 4),
+(29, 'Ricardo Martins', '1996-05-30', 3.60, 5.72, 1),
+(30, 'Juliana Ferreira', '1997-10-09', 8.02, 7.45, 5);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `cursos`
+--
+
+CREATE TABLE `cursos` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(60) NOT NULL,
+  `carga_horaria` int(11) NOT NULL,
+  `professor_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `cursos`
+--
+
+INSERT INTO `cursos` (`id`, `nome`, `carga_horaria`, `professor_id`) VALUES
+(1, 'Front-End', 40, 5),
+(2, 'Back-End', 80, 4),
+(3, 'UX/UI Design', 30, 3),
+(4, 'Adobe XD', 15, 2),
+(5, 'Redes de Computadores', 100, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `professores`
+--
+
+CREATE TABLE `professores` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(120) NOT NULL,
+  `area_atuacao` enum('design','desenvolvimento','infra') NOT NULL,
+  `curso_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `professores`
+--
+
+INSERT INTO `professores` (`id`, `nome`, `area_atuacao`, `curso_id`) VALUES
+(1, 'Jon Oliva', 'infra', 5),
+(2, 'Lemmy Kilmister', 'design', 4),
+(3, 'Neil Peart', 'design', 3),
+(4, 'Ozzy Osbourne', 'desenvolvimento', 2),
+(5, 'David Gilmour', 'desenvolvimento', 1);
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `alunos`
+--
+ALTER TABLE `alunos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_alunos_cursos` (`curso_id`);
+
+--
+-- Índices de tabela `cursos`
+--
+ALTER TABLE `cursos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_cursos_professores` (`professor_id`);
+
+--
+-- Índices de tabela `professores`
+--
+ALTER TABLE `professores`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_professores_cursos` (`curso_id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `alunos`
+--
+ALTER TABLE `alunos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+--
+-- AUTO_INCREMENT de tabela `cursos`
+--
+ALTER TABLE `cursos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de tabela `professores`
+--
+ALTER TABLE `professores`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `alunos`
+--
+ALTER TABLE `alunos`
+  ADD CONSTRAINT `fk_alunos_cursos` FOREIGN KEY (`curso_id`) REFERENCES `cursos` (`id`);
+
+--
+-- Restrições para tabelas `cursos`
+--
+ALTER TABLE `cursos`
+  ADD CONSTRAINT `fk_cursos_professores` FOREIGN KEY (`professor_id`) REFERENCES `professores` (`id`);
+
+--
+-- Restrições para tabelas `professores`
+--
+ALTER TABLE `professores`
+  ADD CONSTRAINT `fk_professores_cursos` FOREIGN KEY (`curso_id`) REFERENCES `cursos` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
